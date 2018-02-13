51a24dbe09d0c5b0a6e219e023a273002ca7d481
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-13 03:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  var cart = getCart();
   var cartContents = "In your cart, you have "
   if (cart.length === 0) {
     return "Your shopping cart is empty";
